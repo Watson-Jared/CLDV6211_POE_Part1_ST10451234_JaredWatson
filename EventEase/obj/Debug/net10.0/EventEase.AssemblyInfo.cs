ac2e0e1001b619ed8e92d0fbffaee081d8ae829a
Watson-Jared <166000001+Watson-Jared@users.noreply.github.com>
2026-03-17 15:14:35 +0200

    FIx Formatting

--- EventEase/obj/Debug/net10.0/EventEase.AssemblyInfo.cs
+++ EventEase/obj/Debug/net10.0/EventEase.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventEase")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f765005e012d9435357ffaa712ef511ffbc317d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b44377dcdd21ddbbb0f1617128f33cf940fa87f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventEase")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventEase")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
